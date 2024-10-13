a4ce18fb501b10da8a21c4911185a64497137b8b
Deniz Simsek <dsimsek@1-C-4.42heilbronn.de>
2024-10-13 17:35:16 +0200

    update 3

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dsimsek <dsimsek@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 18:49:41 by dsimsek           #+#    #+#             */
-/*   Updated: 2024/10/13 16:30:24 by dsimsek          ###   ########.fr       */
+/*   Updated: 2024/10/13 17:17:39 by dsimsek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,4 +29,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+char	*ft_strchr(const char *s, int c);
 #endif
